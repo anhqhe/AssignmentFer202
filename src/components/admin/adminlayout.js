8337ff727273efc4fb6bfa7a27d5ed8b105a413f
khanhduy583 <phamdokhanhduy5803@gmail.com>
2025-12-15 23:09:45 +0700

    add validation, update and fix admin's business logic

--- src/components/admin/adminlayout.js
+++ src/components/admin/adminlayout.js
@@ -3,17 +3,16 @@ import Sidebar from "./Sidebar";
 import { Outlet, useNavigate } from "react-router-dom";
 
 export default function AdminLayout() {
-  
-    return (
-        <Container fluid>
-            <Row>
-                <Col xs={2}>
-                    <Sidebar></Sidebar>
-                </Col>
-                <Col xs={10}>
-                    <Outlet />
-                </Col>
-            </Row>
-        </Container>
-    )
+  return (
+    <Container fluid>
+      <Row>
+        <Col xs={2}>
+          <Sidebar></Sidebar>
+        </Col>
+        <Col xs={10}>
+          <Outlet />
+        </Col>
+      </Row>
+    </Container>
+  );
 }
